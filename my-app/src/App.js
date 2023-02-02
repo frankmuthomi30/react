78463de807a303728fd998cdabae69f5eef6a4a0
frankmuthomi30 <frankmuthomi30@gmail.com>
2023-02-02 09:05:34 +0300

    new app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+<h1>sunset images</h1>
+
+<img src="C:\Users\MUTHOMI\Desktop\jitu training\react ass1\my-app\sunset.jfif"/>
+<img src "C:\Users\MUTHOMI\Desktop\jitu training\react ass1\my-app\download.jfif"/>
+<img src="C:\Users\MUTHOMI\Desktop\jitu training\react ass1\my-app\sunset.jfif"/>
+<img src "C:\Users\MUTHOMI\Desktop\jitu training\react ass1\my-app\download.jfif"/>
+    </div>
+  );
+}
+
+export default App;
